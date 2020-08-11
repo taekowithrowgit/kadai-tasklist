c6630092f34c2201db83fab8254bd6c0071d0aa1
taeko.M <taeko.forest@gmail.com>
2020-08-11 07:50:55 +0000

    kadai-tasklist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Task.create(status: "重要", content: "重要なこと")
+# Task.create(status: "中", content: "テスト作成")
+# Task.create(status: "中", content: "テスト作成")
+# Task.create(status: "中", content: "テスト作成")
+
+(1..100).each do |number|
+  Task.create!(status: 'テストステータス ' + number.to_s, content: 'テスト大量 ' + number.to_s)
+end
